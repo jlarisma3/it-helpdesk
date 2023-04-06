5c73384aae38f42bb30e9276690ca83f587f7617
Joe <dev.joe.larisma.iii@gmail.com>
2023-04-06 08:45:06 +0800

    ticket

---
+++ app/Http/Controllers/Common/Support/SupportGroupController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Common\Support;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SupportGroupController extends Controller
+{
+    //
+}
--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,9 +1,30 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Ticket\TicketController;
+use \App\Http\Controllers\Common\{
+    User\UserController,
+    Branch\BranchController,
+    Branch\DepartmentController,
+    Support\SupportGroupController
+};
 
 Route::get('/', function (\Illuminate\Http\Request $request) {
     return \Inertia\Inertia::render('Pages/Dashboard');
 })->name('dashboard');
 
 Route::resource('ticket', TicketController::class);
+
+Route::prefix('admin')->group(function () {
+    //user
+    Route::resource('user', UserController::class);
+
+    //department
+    Route::resource('branch/department', DepartmentController::class);
+
+    //branch
+    Route::resource('branch', BranchController::class);
+
+    //support
+    Route::resource('support-group', SupportGroupController::class)
+        ->parameters(['support_group' => 'supportGroup']);
+});
